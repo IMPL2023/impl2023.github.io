5975f29ee7216e685539b6b14aac293708a22b91
IMPL2023 <1008384@mymail.sutd.edu.sg>
2024-01-20 05:20:57 +0800

    '202'

--- src/pages/funding.tsx
+++ src/pages/funding.tsx
@@ -2,7 +2,7 @@ import Layout from '@/Layout/Main.Layout';
 import { About, Blog, Contact, Intro, Projects,News } from '@/components';
 import data from '@/data';
 import { PostType } from '@/types';
-import getPosts from '@/utils/getPosts';
+
 import { GetStaticProps } from 'next';
 import { FC } from 'react';
 import { FundingNews } from '@/components';
@@ -20,13 +20,3 @@ const Funding: FC = () => {
 
 export default Funding;
 
-export const getStaticProps: GetStaticProps = async () => {
-  const Posts = await getPosts(data.username);
-
-  return {
-    props: {
-      Posts: Posts.publication.posts,
-    },
-    revalidate: 60 * 60 * 24,
-  };
-};
